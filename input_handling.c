50131df75b609d457006eba9003f7e50d6ed7d33
Carl S <sinocruc@oregonstate.edu>
2020-02-13 19:27:53 -0800

    added header file for input_handing, made GetLine() a function that reads user input from stdin and returns  a string to the caller

--- input_handling.c
+++ input_handling.c
@@ -1,35 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "input_handling.h"
-
-int main(){
-
+/**********************************************************************
+ * GetInput() - reads user input from stdin and returns a string
+ **********************************************************************/
+char* GetInput(){
     int num_chars_entered = -5;
-    int curr_char = -5;
+    //int curr_char = -5;
     size_t input_buffer = 0;
     char* user_input = NULL;
 
-    while(1){
-        while(1){
-            printf("Enter input (CTRL-C to exit): ");
-            num_chars_entered = getline(&user_input, &input_buffer, stdin);
-            if(num_chars_entered == -1){
-                clearerr(stdin);
-            }
-            if(strcmp(user_input, "exit\n") == 0){
-                printf("Exiting\n");
-                free(user_input);
-                exit (0);
-            }
-            else{
-                printf("\tentered: %s\n", user_input);
-                break;
-            }
+    printf("Enter input (CTRL-C to exit): ");
+        fflush(stdout);
+        num_chars_entered = getline(&user_input, &input_buffer, stdin);
+        if(num_chars_entered == -1){ 
+            clearerr(stdin); //clear stdin in case error occurs
+            return NULL;
         }
-        
-    }
-    free(user_input);
-    return 0;
+        else{
+            //replace newline character with null terminator
+            (user_input)[strcspn(user_input, "\n")] = '\0';
+            printf("input: %s[%d]\n", user_input, (int)strlen(user_input)); 
+            fflush(stdout);
+            return user_input;
+        }
 }
+
+            
+            
+ 
+           
+    
+    
--- input_handling.h
+++ input_handling.h
@@ -1,4 +1,10 @@
 #ifndef INPUT_HANDLING
 #define INPUT_HANDLING
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* GetInput();
+
 #endif
