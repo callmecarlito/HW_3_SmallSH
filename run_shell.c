4e2158707a6c78ddad9602a24ace9f0065b9ec18
Carl S <sinocruc@oregonstate.edu>
2020-02-17 19:35:20 -0800

    minor change

--- run_shell.c
+++ run_shell.c
@@ -1,11 +1,16 @@
 #include "input_handling.h"
+#include "built_in_cmnds.h"
 
 int main(){
         char* user_input = NULL;
     while(1){
         user_input = GetInput();
         printf("%s\n", user_input);
-        if(strcmp(user_input, "exit") == 0){
+        if(ValidCmndLength(user_input)){
+            printf("Command is of valid length\n");
+            fflush(stdout);
+        }
+        if(strcmp(user_input, "exit\n") == 0){ /************ REMOVE "\n" **********/
             printf("Exiting\n");
             free(user_input);
             exit(0);
