50e68d203465f686201374c7ff4b1ff859bb427c
Carl S <sinocruc@oregonstate.edu>
2020-02-17 19:34:16 -0800

    GetInput() functioning; building supporting functions for input handling

--- input_handling.c
+++ input_handling.c
@@ -1,4 +1,11 @@
 #include "input_handling.h"
+
+/**********************************************************************
+ *  
+ **********************************************************************/
+void ProcessInput(char* user_input, char* cmnd_args[]){
+
+}
 /**********************************************************************
  * GetInput() - reads user input from stdin and returns a string
  **********************************************************************/
@@ -11,22 +18,52 @@ char* GetInput(){
     printf("Enter input (CTRL-C to exit): ");
         fflush(stdout);
         num_chars_entered = getline(&user_input, &input_buffer, stdin);
-        if(num_chars_entered == -1){ 
+        if(num_chars_entered < 0){ 
             clearerr(stdin); //clear stdin in case error occurs
             return NULL;
         }
         else{
             //replace newline character with null terminator
-            (user_input)[strcspn(user_input, "\n")] = '\0';
             printf("input: %s[%d]\n", user_input, (int)strlen(user_input)); 
             fflush(stdout);
             return user_input;
         }
 }
+/**********************************************************************
+ * ValidCmndLength() - 
+ **********************************************************************/
+bool ValidCmndLength(char* user_input){
+    if(strlen(user_input) < MAX_LENGTH){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+/**********************************************************************
+ *  
+ **********************************************************************/     
+bool IgnoreInput(char* user_input){
 
-            
-            
- 
+}
+/**********************************************************************
+ *  
+ **********************************************************************/
+void RemoveNewline(char* user_input){
+    (user_input)[strcspn(user_input, "\n")] = '\0';
+}
+/**********************************************************************
+ *  
+ **********************************************************************/
+void ParseInput(char* user_input, char* cmnd_args[]){
+    
+}
+/**********************************************************************
+ *  
+ **********************************************************************/
+bool ValidNumArgs(char* cmnd_args[]){
+
+}
            
     
     
--- input_handling.h
+++ input_handling.h
@@ -1,10 +1,20 @@
 #ifndef INPUT_HANDLING
 #define INPUT_HANDLING
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LENGTH 2048
+#define MAX_ARGS 512
+
+void ProcessInput(char* user_input, char* cmnd_args[]);
 char* GetInput();
+bool ValidCmndLength(char* user_input);
+bool IgnoreInput(char* user_input);
+void RemoveNewline(char* user_input);
+void ParseInput(char* user_input, char* cmnd_args[]);
+bool ValidNumArgs(char* cmnd_args[]);
 
 #endif
