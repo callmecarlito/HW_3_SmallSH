806457bf1758b5b062fae1be1b1069b1c68e0443
Carl S <sinocruc@oregonstate.edu>
2020-02-20 15:20:53 -0800

    ChangeDirCmnd() is currently working

--- built_in_cmnds.c
+++ built_in_cmnds.c
@@ -10,7 +10,25 @@ void ExitCmnd(int exit_code){
  * ChangeDirCmnd() - 
  **********************************************************************/
 void ChangeDirCmnd(char* cmnd_args[]){
+    int cd_status;
+    char dir_path[256];
+    //size_t path_buffer = 0;
 
+    if(cmnd_args[1] == NULL || strcmp(cmnd_args[1], "~") == 0){
+        cd_status = chdir(getenv("HOME"));
+        if(cd_status == -1){
+            perror("Change directory error: ");
+        }
+    }
+    else{
+        cd_status = chdir(cmnd_args[1]);
+        if(cd_status == -1){
+            perror("Change directory error: ");
+        }
+    }
+    getcwd(dir_path, sizeof(dir_path));
+    printf("%s\n", dir_path);
+    //free(dir_path);
 }
 /**********************************************************************
  * CurrentStatus() -
--- built_in_cmnds.h
+++ built_in_cmnds.h
@@ -1,7 +1,10 @@
 #ifndef BUILT_IN_CMNDS
 #define BUILT_IN_CMNDS
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 void ExitCmnd(int exit_code);
 void ChangeDirCmnd(char* cmnd_args[]);
